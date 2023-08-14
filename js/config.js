68d05b35dc652ef531e0991c20d61f6dd47245fd
Farnaz <farnazfawadhasan@gmail.com>
2023-08-14 14:46:19 +0600

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g.  ap-south-1_ZqzrulJfN
-        userPoolClientId: '', // e.g. 605ej22e3c30mhmnedcn1jtfkr
-        region: '' // e.g. ap-south-1S
+        userPoolId: 'ap-south-1_ZqzrulJfN', // e.g.  ap-south-1_ZqzrulJfN
+        userPoolClientId: '605ej22e3c30mhmnedcn1jtfkr', // e.g. 605ej22e3c30mhmnedcn1jtfkr
+        region: 'ap-south-1S' // e.g. ap-south-1S
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
